d28bca898d69994d6b12057683910de5d7a55b42
GabrielCardozo92 <gabrielcardozococ@gmail.com>
2024-03-03 16:39:49 -0300

    Segundo commit com alterações de menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,34 @@
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import { useState } from 'react'
 
 
 function Header() {
+
+    const [ ShowMenu, setShowMenu ] = useState(false)
+    const toggleMenu = () => {
+        setShowMenu(!ShowMenu)
+    }
+
     return (
         <header className={styles.header}>
             <Link to="/">
                 <span>GabrielCardozo.dev</span>
             </Link>
-            <nav>
+            <nav 
+                className={`${styles.menuSandwich} ${ ShowMenu ? styles.show : ''}`}
+                onClick={toggleMenu}
+                >
                 <Link to="/">Home</Link>
                 <Link to ="/sobre">Sobre</Link>
                 <Link to="/projetos">Projetos</Link>
                 <Link to="/contatos">Contatos</Link>
             </nav>
+            <div className={styles.menuButton} onClick={toggleMenu}>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+            </div>
         </header>
     )
 }
